4d6a854c9dcd651fe24ea980b26a3109c8486400
SnoopyCodeX <extremeclasher@gmail.com>
2022-03-14 20:38:39 +0800

    yawa

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ commands.add(command.settings.list, {
 });
 
 commands.add(command.say.say, {
-	params: '^say\\s(.*)\\s(.*)',
+	params: '^say\\s([wW]+)\\s([wW]+)',
 	usage:  "say <language> <word/phrase>",
 	description: "Sends an audio recording of the word/phrase",
 	name: "say",
